94a8b419bca0a8a359001e0a7287c3d45cba3cf4
jahanfaytech1 <146122503+jahanfaytech1@users.noreply.github.com>
2024-02-12 13:46:16 -0500

    M3Pro-code (functions).py

--- M3Pro-code (functions).py
+++ M3Pro-code (functions).py
@@ -3,16 +3,19 @@
 # CSC121 M3Pro– List
 # Jahan P
 
-#"ROS" = rate of spread
-def calcI(already_infected, ROS):
-    '''Takes in an integer and a list. return a list'''
-    for item in ROS:
+#functions
 
-def main():
-    
-    num_infected = int(input("Enter how many people are currently infected?: "))
-    rate_no_vac = [3, 3, 3, 3, 3, 3, 3, 3]
-    rate_vac = [3, 3, 3, 3, 0.8, 0.8, 0.8, 0.8]
-    
-if __name__ == "__main__":
-    main()
+def calcI(num_infected, ROS):
+    '''Takes in an integer and a list. Returns a list.'''
+    infections = [num_infected]
+    for rate in ROS:
+        num_infected *= rate
+        infections.append(num_infected)
+    return infections
+
+def display_table(week, scenario1, scenario2):
+    '''Displays the table of results.'''
+    print("Week\tScenario 1\tScenario 2")
+    print("--------------------------------")
+    for i in range(len(week)):
+        print(f"{week[i]}\t{scenario1[i]}\t\t{scenario2[i]}")
