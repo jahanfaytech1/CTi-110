09588aee628d569f0cf995a3332f963ceb7d7a03
jahanfaytech1 <146122503+jahanfaytech1@users.noreply.github.com>
2024-02-12 13:25:05 -0500

    Add files via upload

---
+++ M3Pro-code (1).py
@@ -0,0 +1,18 @@
+# num of infected
+# 2/1/24
+# CSC121 M3Pro– List
+# Jahan P
+
+#"ROS" = rate of spread
+def calcI(already_infected, ROS):
+    '''Takes in an integer and a list. return a list'''
+    for item in ROS:
+
+def main():
+    
+    num_infected = int(input("Enter how many people are currently infected?: "))
+    rate_no_vac = [3, 3, 3, 3, 3, 3, 3, 3]
+    rate_vac = [3, 3, 3, 3, 0.8, 0.8, 0.8, 0.8]
+    
+if __name__ == "__main__":
+    main()
