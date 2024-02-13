9151a443a64a3346a323663499ba6aa1ea3669ec
jahanfaytech1 <146122503+jahanfaytech1@users.noreply.github.com>
2024-02-13 14:24:40 -0500

    M3Pro-code.py

--- M3Pro-code.py
+++ M3Pro-code.py
@@ -10,11 +10,13 @@ def main():
     num_infected = int(input("Enter how many people are currently infected?: "))
     week = list(range(1, 9))
     rate_no_vac = [3, 3, 3, 3, 3, 3, 3, 3]
-    rate_vac = [3, 3, 3, 3, 0.8, 0.8, 0.8, 0.8]
+    rate_vac = [3, 3, 3, 0.8, 0.8, 0.8, 0.8, 0.8]
 
     scenario1 = calcI(num_infected, rate_no_vac)
     scenario2 = calcI(num_infected, rate_vac)
 
     display_table(week, scenario1, scenario2)
+
+    
 if __name__ == "__main__":
     main()
