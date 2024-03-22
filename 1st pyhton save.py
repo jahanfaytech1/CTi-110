5049bac3f6feeced07f3ee44f752a4b9ba09d28e
jahanfaytech1 <146122503+jahanfaytech1@users.noreply.github.com>
2024-03-22 09:43:57 -0400

    Update 1st pyhton save.py

--- 1st pyhton save.py
+++ 1st pyhton save.py
@@ -1,3 +1,8 @@
 username = input("Enter your name: ")
 
 print("Hello", username, "welcome to CTI-110")
+
+'''You may choose any court case that did not go to the Supreme Court. Any State level case is fair game, and it is not limited to cases occurring in North Carolina.
+You will choose a topic
+You will write a minimum of 3 pages detailing the case. What happened, who was involved, where and when did it occur, and any information about evidence you can give me. You will be required to tell me the verdict that the judge came to and the sentence if there was one that was given.
+It can walk us through the story of the court case or it can be an analysis of the case. use simple words that a middle schooler would understand'''
